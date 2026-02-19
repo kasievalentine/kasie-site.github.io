30b5793ed3500bed5b35581614d32c027e97ffa1
Valentine Kasie <kasievalentine07@gmail.com>
2026-02-19 03:27:20 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,9 @@
-const cacheName = 'techadv-cache-v1';
-const filesToCache = [
-  '/',
-  '/index.html',
-  '/about.html',
-  '/contact.html',
-  '/privacy.html',
-  '/styles.css',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
-// Install Service Worker and cache files
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
-  );
+self.addEventListener('install', e => {
+  console.log('Service Worker Installed');
 });
 
-// Fetch requests from cache first
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
